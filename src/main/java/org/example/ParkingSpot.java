5fce27843c02d51cc26f67e1afdc24cdcc226021
tatazoe <takashi.tazoe@gmail.com>
2023-01-24 04:43:11 +0900

    :bug: Update Getter/Setter to correlate DB status

--- src/main/java/org/example/ParkingSpot.java
+++ src/main/java/org/example/ParkingSpot.java
@@ -1,14 +1,30 @@
 package org.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 
 @AllArgsConstructor
-@Data
 public class ParkingSpot {
     private int id;
     private boolean isFree;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public boolean getIsFree() {
+        return isFree;
+    }
+
+    public void setIsFree(boolean a) {
+        isFree = a;
+    }
 
     public ParkingSpot(){
 
